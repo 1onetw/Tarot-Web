17dde2ac7f2c80fea93d79b57bdd7706f26e600c
lunchangzhou <12278949+on-chang-zhou@user.noreply.gitee.com>
2025-07-05 06:48:42 +0800

    塔罗牌占卜 网站布局调整

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="w-full max-w-[888px] px-[16.66px] mt-8 pb-8">
     <main class="m-auto pt-4 flex flex-col-reverse md:flex-row justify-between gap-6 md:gap-16 w-full max-w-[1666px] border-t">
       <section class="[&>p]:text-xs [&>p]:w-full [&>p]:py-[6px] [&>p]:text-center md:[&>p]:text-left [&>p>a]:text-slate-400">
-        <p>塔罗牌占卜 由 <a href="https://www.journy.online/" target="_blank" title="xqq博客">xqq博客</a> 提供技术支持</p>
+        <p>塔罗牌占卜 由 <a href="https://www.journy.online/" target="_blank" title="lcz博客">lcz博客</a> 提供技术支持</p>
         <p>开源地址 <a href="https://github.com/1onetw/Tarot-Web" target="_blank" title="Tarot-Web">Tarot-Web</a></p>
       </section>
       <section class="flex flex-col items-center gap-2">
@@ -13,7 +13,7 @@
           <a title="Github" target="_blank" rel="nofollow" href="https://github.com/1onetw?tab=overview&from=2025-04-01&to=2025-04-25">
             <img class="w-8" src="@/assets/images/footer/github.svg" />
           </a>
-          <a title="韩小韩博客" target="_blank" href="https://www.journy.online/">
+          <a title="lcz博客" target="_blank" href="https://www.journy.online/">
             <img class="w-8" src="@/assets/images/footer/blog.svg" />
           </a>
         </div>
